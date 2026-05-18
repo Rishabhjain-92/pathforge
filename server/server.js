fddbe8d1e43e8e4a12ae0dd61f567d055ae40883
Rishabhjain-92 <rishabhjain92148@gmail.com>
2026-05-18 19:07:18 +0530

    setyup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("PathForge API Running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
